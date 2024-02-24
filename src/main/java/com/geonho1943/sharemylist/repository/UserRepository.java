51d534241c3779518b2403a37011dd6519370fcd
geonho1943 <thsrjsgh02@naver.com>
2024-02-24 17:44:45 +0900

    fix: 로그인시 비밀번호 hash,salt 대응
    회원가입시 비밀번호 해시처리후, 로그인시 비밀번호 대조를 위해
    UserService userLogin에 hash,salt 기능 추가

--- src/main/java/com/geonho1943/sharemylist/repository/UserRepository.java
+++ src/main/java/com/geonho1943/sharemylist/repository/UserRepository.java
@@ -7,4 +7,6 @@ import org.springframework.stereotype.Repository;
 @Repository
 public interface UserRepository extends JpaRepository<User, Long> {
     User findByUserIdAndUserPw(String userId, String userPw);
+
+    User findUserSaltByUserId(String userId);
 }
--- src/main/java/com/geonho1943/sharemylist/service/UserService.java
+++ src/main/java/com/geonho1943/sharemylist/service/UserService.java
@@ -6,29 +6,47 @@ import com.geonho1943.sharemylist.repository.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
+import java.security.SecureRandom;
+import java.util.Base64;
+
 @Service
 public class UserService {
     @Autowired
     private UserRepository userRepository;
 
     public UserDto userLogin(UserDto loginInfo) {
-        User loginInfoEntity = new User(loginInfo.getUserId(), loginInfo.getUserPw());
-        //Entity의 생성자를 사용하여 DTO를 Entity로 변경
+        // 로그인
+        User storedSalt = userRepository.findUserSaltByUserId(loginInfo.getUserId());
+        // salt 조회
+
+        String hashedPassword = hashPassword(loginInfo.getUserPw(), storedSalt.getUserSalt());
+        // 입력된 비밀번호와 저장된 salt를 사용하여 해싱
+
+        User loginInfoEntity = new User(loginInfo.getUserId(), hashedPassword);
+        // Entity의 생성자를 사용하여 DTO를 Entity로 변경
+
         User checkedUserInfoEntity = userRepository.findByUserIdAndUserPw(
                 loginInfoEntity.getUserId(), loginInfoEntity.getUserPw());
-        //DB에 유저정보 대조
+        // DB에 유저정보 대조
+
+        // 대조된 유저정보가 없으면 예외 발생
         if (checkedUserInfoEntity == null) {
             throw new IllegalArgumentException("입력된 정보와 일치하는 사용자가 없습니다.");
         }
+        // 계정이 비활성화되어 있으면 예외 발생
         if (!checkedUserInfoEntity.isUserStatus()) {
             throw new RuntimeException("비활성화된 계정입니다. 로그인이 불가능합니다.");
         }
-        return new UserDto(
-                checkedUserInfoEntity.getUserIdx(), checkedUserInfoEntity.getUserId(),
-                checkedUserInfoEntity.getUserName(), checkedUserInfoEntity.getUserReg(),
-                checkedUserInfoEntity.getUserPrivileges());
-        //Entity를 Dto로 변환하여 반환
+
+    // Entity를 Dto로 반환
+    return new UserDto(
+        checkedUserInfoEntity.getUserIdx(), checkedUserInfoEntity.getUserId(),
+        checkedUserInfoEntity.getUserName(), checkedUserInfoEntity.getUserReg(),
+        checkedUserInfoEntity.getUserPrivileges(), checkedUserInfoEntity.getUserSalt());
     }
+
 
     public void resgin(UserDto resignInfo) {
         //회원탈퇴
